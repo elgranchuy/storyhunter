7dbae75cc9ca31f0ecc48c823ac6b9f190540266
Jesus Dominguez <jesusgda84@gmail.com>
2022-09-27 15:21:05 -0500

    add tags relationship

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Thread extends Model
@@ -21,5 +22,10 @@ class Thread extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->BelongsToMany(Tag::class);
     }
 }
