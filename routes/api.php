f5da59a6e12e522e6bf8c737718bcdb47111e254
Jesus Dominguez <jesusgda84@gmail.com>
2022-09-27 15:21:16 -0500

    add tag endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\ThreadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -20,17 +21,27 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [RegisterController::class, 'login']);
 
+// Add routes for user endpoints
 Route::middleware(['auth:api'])->controller(UserController::class)->group(function () {
     Route::get('/users/{id}', 'show');
     Route::post('/users', 'store');
 });
 
+// Add routes for thread endpoints
 Route::middleware(['auth:api'])->controller(ThreadController::class)->group(function () {
     Route::get('/threads/{user_id}', 'index');
     Route::post('/threads', 'store');
+    Route::patch('/threads/{id}', 'update');
 });
 
+// Add routes for message endpoints
 Route::middleware(['auth:api'])->controller(MessageController::class)->group(function () {
     Route::get('/messages/{thread_id}', 'index');
     Route::post('/messages', 'store');
+});
+
+// Add routes for tag endpoints
+Route::middleware(['auth:api'])->controller(TagController::class)->group(function () {
+    Route::get('/tags', 'index');
+    Route::post('/tags', 'store');
 });
